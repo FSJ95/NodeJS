1928009761fec340a83935483b77cdfe1ef7af2c
Frederik Søndergaard Jensen <FSJ95@hotmail.dk>
2020-02-06 09:48:33 +0100

    Basics Exercise

---
+++ Intro/Intro_Exercises/Basics.js
@@ -0,0 +1,13 @@
+//Exercise 1
+var firstName = "Anders";
+var lastName = "Latif";
+
+console.log("My first name is", firstName, "and my last name is", lastName);
+
+//Exercise 2
+var year = "2017";
+var number = 1;
+
+var newYear = parseInt(year) + number
+//var newYear = +(year) + 1;
+console.log(newYear);
